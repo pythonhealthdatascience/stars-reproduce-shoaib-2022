dcf15d650ef071f136a0b295458b8f9be9882217
amyheather <a.heather2@exeter.ac.uk>
2024-07-01 16:19:25 +0100

    ci(compendium): :sparkles: add bash script that runs all notebooks

--- reproduction/run_reproduction.sh
+++ reproduction/run_reproduction.sh
@@ -1,17 +1,23 @@
 # This script will execute all the .ipynb files sequentially
-
-# If you want to run the nbconvert commands directly in terminal in VSCode,
-# make sure you have set interpreter: Ctrl + Shift + P + Jupyter: Select
-# Interpreter to Start Jupyter Server
+# To run it, use the command bash run_reproduction.sh in your terminal
 
 # Activate conda/mamba environment (assumes you have installed)
-# eval "$(conda shell.bash hook)"
-# conda activate shoaib2022
+source activate shoaib2022
 
-# source activate shoaib2022
-# python -m ipykernel install --user --name shoaib2022
-# source deactivate
-# jupyter kernelspec list
+# Add to kernel list (required by nbconvert), and print list of kernels
+# (to confirm it has been add)
+python -m ipykernel install --user --name shoaib2022
+jupyter kernelspec list
+
+# Deactivate that (as nbconvert will use kernel_name rather than current env)
+source deactivate
 
 # Run notebooks
 jupyter nbconvert --to notebook --ExecutePreprocessor.kernel_name=shoaib2022 --execute --inplace scripts/reproduce_tab6.ipynb
+jupyter nbconvert --to notebook --ExecutePreprocessor.kernel_name=shoaib2022 --execute --inplace scripts/reproduce_fig2.ipynb
+jupyter nbconvert --to notebook --ExecutePreprocessor.kernel_name=shoaib2022 --execute --inplace scripts/reproduce_fig3_txt1.ipynb
+jupyter nbconvert --to notebook --ExecutePreprocessor.kernel_name=shoaib2022 --execute --inplace scripts/reproduce_fig4.ipynb
+jupyter nbconvert --to notebook --ExecutePreprocessor.kernel_name=shoaib2022 --execute --inplace scripts/reproduce_txt2.ipynb
+jupyter nbconvert --to notebook --ExecutePreprocessor.kernel_name=shoaib2022 --execute --inplace scripts/reproduce_txt34.ipynb
+jupyter nbconvert --to notebook --ExecutePreprocessor.kernel_name=shoaib2022 --execute --inplace scripts/reproduce_txt5.ipynb
+jupyter nbconvert --to notebook --ExecutePreprocessor.kernel_name=shoaib2022 --execute --inplace scripts/reproduce_txt67.ipynb
